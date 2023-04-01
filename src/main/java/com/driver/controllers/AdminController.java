4c514fe6d1ff8443f7a6f72e0ae1c80bd7caa169
Akash Sharma <akash.bohareji@gmail.com>
2023-04-01 19:26:49 +0530

    Update AdminController.java

--- src/main/java/com/driver/controllers/AdminController.java
+++ src/main/java/com/driver/controllers/AdminController.java
@@ -1,18 +1,26 @@
 package com.driver.controllers;
 
-import com.driver.services.AdminService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.driver.model.*;
-
-
-import java.util.List;
+import com.driver.services.AdminService;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+
 	@Autowired
 	AdminService adminService;
 
@@ -23,13 +31,13 @@ public class AdminController {
 	}
 
 	@PutMapping("/update")
-	public ResponseEntity<Admin> updateAdminPassword(@RequestParam Integer adminId, @RequestParam String password){
-		Admin updatedAdmin= adminService.updatePassword(adminId, password);
+	public ResponseEntity<Admin> updateAdminPassword(@PathVariable Integer adminId, @PathVariable String password){
+		Admin updatedAdmin = adminService.updatePassword(adminId, password);
 		return new ResponseEntity<>(updatedAdmin, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/delete")
-	public void deleteAdmin(@RequestParam Integer adminId){
+	public void deleteAdmin(@PathVariable Integer adminId){
 		adminService.deleteAdmin(adminId);
 	}
 
