42be4d20b5ee26fe6a6cc9375ad2d1b538992007
Akash Sharma <akash.bohareji@gmail.com>
2023-04-01 19:29:51 +0530

    Update Admin.java

--- src/main/java/com/driver/model/Admin.java
+++ src/main/java/com/driver/model/Admin.java
@@ -1,18 +1,15 @@
 package com.driver.model;
 
-
-import net.bytebuddy.dynamic.loading.InjectionClassLoader;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 @Entity
-public class Admin{
+public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer adminId;
+
     private String username;
     private String password;
 
